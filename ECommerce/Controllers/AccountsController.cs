10b3101347caffe79370c64b2c69f38d69e0ed25
akademibasaran <akademibasaran@gmail.com>
2024-11-22 19:53:10 +0300

    added register

--- ECommerce/Controllers/AccountsController.cs
+++ ECommerce/Controllers/AccountsController.cs
@@ -1,8 +1,10 @@
 ﻿using ECommerce.Data;
+using ECommerce.Data.Static;
 using ECommerce.Models;
 using ECommerce.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 
 namespace ECommerce.Controllers;
 
@@ -22,7 +24,7 @@ public class AccountsController : Controller
     public IActionResult Login() => View(new LoginVM());
 
     [HttpPost]
-    public async Task<IActionResult> Login(LoginVM login) 
+    public async Task<IActionResult> Login(LoginVM login)
     {
         if (!ModelState.IsValid) return View(login);
 
@@ -31,11 +33,11 @@ public class AccountsController : Controller
         if (user is not null)
         {
             var passwordCheck = await _userManager.CheckPasswordAsync(user, login.Password);
-            
+
             if (passwordCheck)
             {
                 var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
-                if (result.Succeeded) 
+                if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "Movies");
                 }
@@ -49,5 +51,47 @@ public class AccountsController : Controller
         return View(login);
 
     }
-    
+
+
+    public IActionResult Register() => View(new RegisterVM());
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVM register)
+    {
+        if (!ModelState.IsValid) return View(register);
+     
+        var user = await _userManager.FindByEmailAsync(register.EmailAddress);
+
+        if (user is not null)
+        {
+            TempData["Error"] = "Bu email adresi kullanılmaktadır.";
+            return View(register);
+        }
+
+        var newUser = new ApplicationUser() 
+        {
+            Email = register.EmailAddress,
+            FullName = register.FullName,
+            UserName = register.EmailAddress,
+            EmailConfirmed = true
+        };
+
+        var response = await _userManager.CreateAsync(newUser,register.Password);
+
+        if (response.Succeeded)
+            await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+
+        return View("RegisterCompleted");
+
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index","Movies");
+    }
+
 }
---
+++ ECommerce/ViewModels/RegisterVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.ViewModels;
+
+public class RegisterVM
+{
+    [Required(ErrorMessage = "Ad soyad gereklidir.")]
+    [Display(Name = "Ad Soyad")]
+    public string FullName { get; set; }
+
+    [Required(ErrorMessage = "EPosta adresi gereklidir.")]
+    [Display(Name = "EPosta Adresi")]
+    public string EmailAddress { get; set; }
+
+    [Required(ErrorMessage = "Şifre gereklidir.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Şifre")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Şifre tekrarı gereklidir.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Şifre tekrar")]
+    [Compare("Password",ErrorMessage = "Şifreler uyuşmuyor.")]
+    public string ConfirmePassword { get; set; }
+}
